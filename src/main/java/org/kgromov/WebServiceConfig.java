e376a1453a871f6c10686cc094995ed417961baf
konstantin <konst.gromov@gmail.com>
2025-10-06 00:34:04 +0300

    Add missed configuration for WebServiceConfig

--- src/main/java/org/kgromov/WebServiceConfig.java
+++ src/main/java/org/kgromov/WebServiceConfig.java
@@ -6,14 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.server.endpoint.interceptor.PayloadLoggingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EnableWs
 @Configuration
-public class WebServiceConfig {
+public class WebServiceConfig extends WsConfigurerAdapter {
 
     @Bean
     public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
@@ -24,17 +30,23 @@ public class WebServiceConfig {
     }
 
     @Bean(name = "persons")
-    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema personSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("PersonServicePort");
         wsdl11Definition.setLocationUri("/ws");
         wsdl11Definition.setTargetNamespace("http://example.com/personservice");
-        wsdl11Definition.setSchema(countriesSchema);
+        wsdl11Definition.setSchema(personSchema);
         return wsdl11Definition;
     }
 
     @Bean
-    public XsdSchema countriesSchema() {
+    public XsdSchema personSchema() {
         return new SimpleXsdSchema(new ClassPathResource("Person.xsd"));
+    }
+
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        super.addInterceptors(new ArrayList<>(List.of(new PayloadLoggingInterceptor())));
     }
 }
